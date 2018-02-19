09b78b898b20e60dbc89eed7f5de2e92fe8815d4
HiggsInc <adrian@adrianhiggs.com>
2018-02-20 00:08:21 +0200

    minor update to clearplot task

--- src/MyPlot/task/ClearPlotTask.php
+++ src/MyPlot/task/ClearPlotTask.php
@@ -1,22 +1,29 @@
 <?php
+declare(strict_types=1);
 namespace MyPlot\task;
 
+use MyPlot\MyPlot;
+use MyPlot\Plot;
 use pocketmine\block\Block;
 use pocketmine\math\Vector3;
 use pocketmine\Player;
 use pocketmine\scheduler\PluginTask;
 
-use MyPlot\MyPlot;
-use MyPlot\Plot;
-
 class ClearPlotTask extends PluginTask {
 	/** @var MyPlot $plugin */
 	private $plugin;
+	private $plot, $level, $height, $bottomBlock, $plotFillBlock, $plotFloorBlock, $plotBeginPos, $xMax, $zMax, $maxBlocksPerTick, $pos;
 
-	private $level, $height, $bottomBlock, $plotFillBlock, $plotFloorBlock, $plotBeginPos, $xMax, $zMax, $maxBlocksPerTick, $pos;
-
-	public function __construct(MyPlot $plugin, Plot $plot, $maxBlocksPerTick = 256) {
+	/**
+	 * ClearPlotTask constructor.
+	 *
+	 * @param MyPlot $plugin
+	 * @param Plot $plot
+	 * @param int $maxBlocksPerTick
+	 */
+	public function __construct(MyPlot $plugin, Plot $plot, int $maxBlocksPerTick = 256) {
 		parent::__construct($plugin);
+		$this->plot = $plot;
 		$this->plotBeginPos = $plugin->getPlotPosition($plot);
 		$this->level = $this->plotBeginPos->getLevel();
 		$plotLevel = $plugin->getLevelSettings($plot->levelName);
@@ -30,37 +37,38 @@ class ClearPlotTask extends PluginTask {
 		$this->maxBlocksPerTick = $maxBlocksPerTick;
 		$this->pos = new Vector3($this->plotBeginPos->x, 0, $this->plotBeginPos->z);
 		$this->plugin = $plugin;
-		$this->plugin->getLogger()->debug("Clear Task started at plot {$plot->X};{$plot->Z}");
+		$plugin->getLogger()->debug("Clear Task started at plot {$plot->X};{$plot->Z}");
 	}
 
-	public function onRun(int $currentTick) {
-		foreach ($this->level->getEntities() as $entity) {
-			if (($plot = $this->plugin->getPlotByPosition($entity)) != null) {
-				if ($plot->X === $this->plotBeginPos->x and $plot->Z === $this->plotBeginPos->z) {
-					if (!$entity instanceof Player) {
-						$entity->close();
-					}else{
-						$this->plugin->teleportPlayerToPlot($entity, $plot);
-					}
+	/**
+	 * @param int $currentTick
+	 */
+	public function onRun(int $currentTick) : void {
+		foreach($this->level->getEntities() as $entity) {
+			if($this->plugin->getPlotBB($this->plot)->isVectorInXZ($entity)) {
+				if(!$entity instanceof Player) {
+					$entity->close();
+				}else{
+					$this->plugin->teleportPlayerToPlot($entity, $this->plot);
 				}
 			}
 		}
 		$blocks = 0;
-		while ($this->pos->x < $this->xMax) {
-			while ($this->pos->z < $this->zMax) {
-				while ($this->pos->y < 128) {
-					if ($this->pos->y === 0) {
+		while($this->pos->x < $this->xMax) {
+			while($this->pos->z < $this->zMax) {
+				while($this->pos->y < $this->level->getWorldHeight()) {
+					if($this->pos->y === 0) {
 						$block = $this->bottomBlock;
-					} elseif ($this->pos->y < $this->height) {
+					}elseif($this->pos->y < $this->height) {
 						$block = $this->plotFillBlock;
-					} elseif ($this->pos->y === $this->height) {
+					}elseif($this->pos->y === $this->height) {
 						$block = $this->plotFloorBlock;
-					} else {
+					}else{
 						$block = Block::get(0);
 					}
 					$this->level->setBlock($this->pos, $block, false, false);
 					$blocks++;
-					if ($blocks === $this->maxBlocksPerTick) {
+					if($blocks >= $this->maxBlocksPerTick) {
 						$this->getOwner()->getServer()->getScheduler()->scheduleDelayedTask($this, 1);
 						return;
 					}
@@ -72,9 +80,9 @@ class ClearPlotTask extends PluginTask {
 			$this->pos->z = $this->plotBeginPos->z;
 			$this->pos->x++;
 		}
-		foreach ( $this->level->getTiles() as $tile) {
-			if (($plot = $this->plugin->getPlotByPosition($tile)) != null) {
-				if ($plot->X === $this->plotBeginPos->x and $plot->Z === $this->plotBeginPos->z) {
+		foreach($this->level->getTiles() as $tile) {
+			if(($plot = $this->plugin->getPlotByPosition($tile)) != null) {
+				if($plot->X === $this->plotBeginPos->x and $plot->Z === $this->plotBeginPos->z) {
 					$tile->close();
 				}
 			}
