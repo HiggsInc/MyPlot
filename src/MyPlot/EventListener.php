de5530b7f089d739b6c96b65054aff4dd2625727
HiggsInc <adrian@adrianhiggs.com>
2018-02-20 00:06:32 +0200

    minor change

--- src/MyPlot/EventListener.php
+++ src/MyPlot/EventListener.php
@@ -1,28 +1,32 @@
 <?php
+declare(strict_types=1);
 namespace MyPlot;
 
-use pocketmine\block\Liquid;
 use pocketmine\block\Sapling;
-use pocketmine\event\player\PlayerMoveEvent;
-use pocketmine\Player;
-use pocketmine\event\block\BlockUpdateEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
 use pocketmine\event\entity\EntityExplodeEvent;
 use pocketmine\event\entity\EntityMotionEvent;
-use pocketmine\event\Listener;
 use pocketmine\event\level\LevelLoadEvent;
 use pocketmine\event\level\LevelUnloadEvent;
-use pocketmine\utils\Config;
-use pocketmine\event\block\BlockPlaceEvent;
-use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\Player;
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class EventListener implements Listener
 {
-	/** @var MyPlot */
+	/** @var MyPlot $plugin */
 	private $plugin;
 
-	public function __construct(MyPlot $plugin){
+	/**
+	 * EventListener constructor.
+	 *
+	 * @param MyPlot $plugin
+	 */
+	public function __construct(MyPlot $plugin) {
 		$this->plugin = $plugin;
 	}
 
@@ -31,30 +35,23 @@ class EventListener implements Listener
 	 *
 	 * @param LevelLoadEvent $event
 	 */
-	public function onLevelLoad(LevelLoadEvent $event) {
-		if ($event->getLevel()->getProvider()->getGenerator() == "myplot") {
-			$this->plugin->getLogger()->debug("MyPlot level ".$event->getLevel()->getFolderName()." loaded!");
+	public function onLevelLoad(LevelLoadEvent $event) : void {
+		if($event->getLevel()->getProvider()->getGenerator() === "myplot") {
+			$this->plugin->getLogger()->debug("MyPlot level " . $event->getLevel()->getFolderName() . " loaded!");
 			$settings = $event->getLevel()->getProvider()->getGeneratorOptions();
-			if (!isset($settings["preset"]) or empty($settings["preset"])) {
+			if(!isset($settings["preset"]) or empty($settings["preset"])) {
 				return;
 			}
 			$settings = json_decode($settings["preset"], true);
-			if ($settings === false) {
+			if($settings === false) {
 				return;
 			}
-			$levelName = $event->getLevel()->getName();
+			$levelName = $event->getLevel()->getFolderName();
 			$filePath = $this->plugin->getDataFolder() . "worlds" . DIRECTORY_SEPARATOR . $levelName . ".yml";
 			$config = $this->plugin->getConfig();
-			$default = [
-				"RestrictEntityMovement" => $config->getNested("DefaultWorld.RestrictEntityMovement", true),
-				"UpdatePlotLiquids" => $config->getNested("DefaultWorld.UpdatePlotLiquids", false),
-				"ClaimPrice" => $config->getNested("DefaultWorld.ClaimPrice", 0),
-				"ClearPrice" => $config->getNested("DefaultWorld.ClearPrice", 0),
-				"DisposePrice" => $config->getNested("DefaultWorld.DisposePrice", 0),
-				"ResetPrice" => $config->getNested("DefaultWorld.ResetPrice", 0)
-			];
+			$default = ["RestrictEntityMovement" => $config->getNested("DefaultWorld.RestrictEntityMovement", true), "UpdatePlotLiquids" => $config->getNested("DefaultWorld.UpdatePlotLiquids", false), "ClaimPrice" => $config->getNested("DefaultWorld.ClaimPrice", 0), "ClearPrice" => $config->getNested("DefaultWorld.ClearPrice", 0), "DisposePrice" => $config->getNested("DefaultWorld.DisposePrice", 0), "ResetPrice" => $config->getNested("DefaultWorld.ResetPrice", 0)];
 			$config = new Config($filePath, Config::YAML, $default);
-			foreach (array_keys($default) as $key) {
+			foreach(array_keys($default) as $key) {
 				$settings[$key] = $config->get($key);
 			}
 			$this->plugin->addLevelSettings($levelName, new PlotLevelSettings($levelName, $settings));
@@ -67,13 +64,13 @@ class EventListener implements Listener
 	 *
 	 * @param LevelUnloadEvent $event
 	 */
-	public function onLevelUnload(LevelUnloadEvent $event) {
+	public function onLevelUnload(LevelUnloadEvent $event) : void {
 		if($event->isCancelled()) {
 			return;
 		}
-		$levelName = $event->getLevel()->getName();
+		$levelName = $event->getLevel()->getFolderName();
 		if($this->plugin->unloadLevelSettings($levelName)) {
-			$this->plugin->getLogger()->debug("Level ".$event->getLevel()->getFolderName()." unloaded!");
+			$this->plugin->getLogger()->debug("Level " . $event->getLevel()->getFolderName() . " unloaded!");
 		}
 	}
 
@@ -83,7 +80,7 @@ class EventListener implements Listener
 	 *
 	 * @param BlockPlaceEvent $event
 	 */
-	public function onBlockPlace(BlockPlaceEvent $event) {
+	public function onBlockPlace(BlockPlaceEvent $event) : void {
 		$this->onEventOnBlock($event);
 	}
 
@@ -93,7 +90,7 @@ class EventListener implements Listener
 	 *
 	 * @param BlockBreakEvent $event
 	 */
-	public function onBlockBreak(BlockBreakEvent $event) {
+	public function onBlockBreak(BlockBreakEvent $event) : void {
 		$this->onEventOnBlock($event);
 	}
 
@@ -103,26 +100,26 @@ class EventListener implements Listener
 	 *
 	 * @param PlayerInteractEvent $event
 	 */
-	public function onPlayerInteract(PlayerInteractEvent $event) {
+	public function onPlayerInteract(PlayerInteractEvent $event) : void {
 		$this->onEventOnBlock($event);
 	}
 
 	/**
 	 * @param BlockPlaceEvent|BlockBreakEvent|PlayerInteractEvent $event
 	 */
-	private function onEventOnBlock($event) {
+	private function onEventOnBlock($event) : void {
 		if($event->isCancelled()) {
 			return;
 		}
-		$levelName = $event->getBlock()->getLevel()->getName();
-		if (!$this->plugin->isLevelLoaded($levelName)) {
+		$levelName = $event->getBlock()->getLevel()->getFolderName();
+		if(!$this->plugin->isLevelLoaded($levelName)) {
 			return;
 		}
 		$plot = $this->plugin->getPlotByPosition($event->getBlock());
-		if ($plot !== null) {
+		if($plot !== null) {
 			$username = $event->getPlayer()->getName();
-			if ($plot->owner == $username or $plot->isHelper($username) or $plot->isHelper("*") or $event->getPlayer()->hasPermission("myplot.admin.build.plot")) {
-				if (!($event instanceof PlayerInteractEvent and $event->getBlock() instanceof Sapling))
+			if($plot->owner == $username or $plot->isHelper($username) or $plot->isHelper("*") or $event->getPlayer()->hasPermission("myplot.admin.build.plot")) {
+				if(!($event instanceof PlayerInteractEvent and $event->getBlock() instanceof Sapling))
 					return;
 
 				/*
@@ -138,37 +135,14 @@ class EventListener implements Listener
 				$plotSize = $this->plugin->getLevelSettings($levelName)->plotSize;
 				$endPos->x += $plotSize - $maxLengthLeaves;
 				$endPos->z += $plotSize - $maxLengthLeaves;
-
-				if ($block->x >= $beginPos->x and $block->z >= $beginPos->z and $block->x < $endPos->x and $block->z < $endPos->z) {
+				if($block->x >= $beginPos->x and $block->z >= $beginPos->z and $block->x < $endPos->x and $block->z < $endPos->z) {
 					return;
 				}
 			}
-		} elseif ($event->getPlayer()->hasPermission("myplot.admin.build.road")) {
+		}elseif($event->getPlayer()->hasPermission("myplot.admin.build.road"))
 			return;
-		}
 		$event->setCancelled();
-		$this->plugin->getLogger()->debug("Road block placement cancelled");
-	}
-
-	/**
-	 * @ignoreCancelled false
-	 * @priority LOWEST
-	 *
-	 * @param BlockUpdateEvent $event
-	 */
-	public function onBlockUpdate(BlockUpdateEvent $event) {
-		if($event->isCancelled()){
-			return;
-		}
-		$levelName = $event->getBlock()->getLevel()->getName();
-		if ($this->plugin->isLevelLoaded($levelName)) {
-			if ($event->getBlock() instanceof Liquid) {
-				if ($this->plugin->getLevelSettings($levelName)->updatePlotLiquids and is_null($this->plugin->getPlotByPosition($event->getBlock()))) {
-					$event->setCancelled();
-					$this->plugin->getLogger()->debug("Block update cancelled in ".$levelName);
-				}
-			}
-		}
+		$this->plugin->getLogger()->debug("Block placement cancelled");
 	}
 
 	/**
@@ -177,16 +151,16 @@ class EventListener implements Listener
 	 *
 	 * @param EntityExplodeEvent $event
 	 */
-	public function onExplosion(EntityExplodeEvent $event) {
+	public function onExplosion(EntityExplodeEvent $event) : void {
 		if($event->isCancelled()) {
 			return;
 		}
-		$levelName = $event->getEntity()->getLevel()->getName();
-		if (!$this->plugin->isLevelLoaded($levelName))
+		$levelName = $event->getEntity()->getLevel()->getFolderName();
+		if(!$this->plugin->isLevelLoaded($levelName))
 			return;
 
 		$plot = $this->plugin->getPlotByPosition($event->getPosition());
-		if ($plot === null) {
+		if($plot === null) {
 			$event->setCancelled();
 			return;
 		}
@@ -195,8 +169,8 @@ class EventListener implements Listener
 		$plotSize = $this->plugin->getLevelSettings($levelName)->plotSize;
 		$endPos->x += $plotSize;
 		$endPos->z += $plotSize;
-		$blocks = array_filter($event->getBlockList(), function($block) use($beginPos, $endPos) {
-			if ($block->x >= $beginPos->x and $block->z >= $beginPos->z and $block->x < $endPos->x and $block->z < $endPos->z) {
+		$blocks = array_filter($event->getBlockList(), function($block) use ($beginPos, $endPos) {
+			if($block->x >= $beginPos->x and $block->z >= $beginPos->z and $block->x < $endPos->x and $block->z < $endPos->z) {
 				return true;
 			}
 			return false;
@@ -210,18 +184,18 @@ class EventListener implements Listener
 	 *
 	 * @param EntityMotionEvent $event
 	 */
-	public function onEntityMotion(EntityMotionEvent $event) {
+	public function onEntityMotion(EntityMotionEvent $event) : void {
 		if($event->isCancelled()) {
 			return;
 		}
-		$levelName = $event->getEntity()->getLevel()->getName();
-		if (!$this->plugin->isLevelLoaded($levelName))
+		$levelName = $event->getEntity()->getLevel()->getFolderName();
+		if(!$this->plugin->isLevelLoaded($levelName))
 			return;
 
 		$settings = $this->plugin->getLevelSettings($levelName);
-		if ($settings->restrictEntityMovement and !($event->getEntity() instanceof Player)) {
+		if($settings->restrictEntityMovement and !($event->getEntity() instanceof Player)) {
 			$event->setCancelled();
-			$this->plugin->getLogger()->debug("Cancelled entity motion on ".$levelName);
+			$this->plugin->getLogger()->debug("Cancelled entity motion on " . $levelName);
 		}
 	}
 
@@ -231,43 +205,39 @@ class EventListener implements Listener
 	 *
 	 * @param PlayerMoveEvent $event
 	 */
-	public function onPlayerMove(PlayerMoveEvent $event) {
+	public function onPlayerMove(PlayerMoveEvent $event) : void {
 		if($event->isCancelled()) {
 			return;
 		}
-		if (!$this->plugin->getConfig()->get("ShowPlotPopup", true))
+		if(!$this->plugin->getConfig()->get("ShowPlotPopup", true))
 			return;
-
-		$levelName = $event->getPlayer()->getLevel()->getName();
-		if (!$this->plugin->isLevelLoaded($levelName))
+		$levelName = $event->getPlayer()->getLevel()->getFolderName();
+		if(!$this->plugin->isLevelLoaded($levelName))
 			return;
 
 		$plot = $this->plugin->getPlotByPosition($event->getTo());
-		if ($plot !== null and $plot !== $this->plugin->getPlotByPosition($event->getFrom())) {
+		if($plot !== null and $plot !== $this->plugin->getPlotByPosition($event->getFrom())) {
 			if($plot->isDenied($event->getPlayer()->getName())) {
 				$event->setCancelled();
 				return;
 			}
-			$plotName = TextFormat::GREEN . $plot;
-			$popup = $this->plugin->getLanguage()->translateString("popup", [$plotName]);
-			if(strpos($plot,"-0")) {
+			if(strpos((string) $plot, "-0")) {
 				return;
 			}
-			if ($plot->owner != "") {
+			$popup = $this->plugin->getLanguage()->translateString("popup", [TextFormat::GREEN . $plot]);
+			if($plot->owner !== "") {
 				$owner = TextFormat::GREEN . $plot->owner;
 				$ownerPopup = $this->plugin->getLanguage()->translateString("popup.owner", [$owner]);
-				$paddingSize = floor((strlen($popup) - strlen($ownerPopup)) / 2);
+				$paddingSize = (int) floor((strlen($popup) - strlen($ownerPopup)) / 2);
 				$paddingPopup = str_repeat(" ", max(0, -$paddingSize));
 				$paddingOwnerPopup = str_repeat(" ", max(0, $paddingSize));
-				$popup = TextFormat::WHITE . $paddingPopup . $popup . "\n" .
-					TextFormat::WHITE . $paddingOwnerPopup . $ownerPopup;
-			} else {
+				$popup = TextFormat::WHITE . $paddingPopup . $popup . "\n" . TextFormat::WHITE . $paddingOwnerPopup . $ownerPopup;
+			}else{
 				$ownerPopup = $this->plugin->getLanguage()->translateString("popup.available");
-				$paddingSize = floor((strlen($popup) - strlen($ownerPopup)) / 2);
+				$paddingSize = (int) floor((strlen($popup) - strlen($ownerPopup)) / 2);
 				$paddingPopup = str_repeat(" ", max(0, -$paddingSize));
 				$paddingOwnerPopup = str_repeat(" ", max(0, $paddingSize));
-				$popup = TextFormat::WHITE . $paddingPopup . $popup . "\n" .
-					TextFormat::WHITE . $paddingOwnerPopup . $ownerPopup;
+				$popup = TextFormat::WHITE . $paddingPopup . $popup . "\n" . TextFormat::WHITE . $paddingOwnerPopup . $ownerPopup;
 			}
 			$event->getPlayer()->sendTip($popup);
 		}
